5e030750351573ccb7103bd133d1a91a3a0dbb39
César Encarnación <cesariux23@gmail.com>
2016-08-08 20:42:30 -0500

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,5 +6,15 @@
     @yield('content')
     @include('layouts.footer')
     @yield('scripts')
+    <footer>
+      <hr>
+      <p class="text-center">
+        <b>IVEA</b>
+        <br>
+        Instituto Veracruzano de Educación para los adultos
+        <br>
+        Sistema de validación de registros del SIGA, desarrollado en el Depto. de TI. por César Encarnación Mendoza, <b>2016</b>
+      </p>
+    </footer>
   </body>
 </html>
