8e08a2dee5533f34209ce232bdb94d95ba83a342
César Encarnación <cesariux23@gmail.com>
2016-08-26 10:33:51 -0500

    Resource para sockets _2

--- resources/views/base/index.blade.php
+++ resources/views/base/index.blade.php
@@ -118,7 +118,7 @@
 @section('scripts')
 <script src="/sockets/socket.io/socket.io.js"></script>
     <script>
-      var socket = io.connect({resource: '/sockets'});
+      var socket = io.connect('http://localhost', {resource: 'sockets'});
 
       socket.on('cambia estado', function(registro) {
         console.log(registro);
